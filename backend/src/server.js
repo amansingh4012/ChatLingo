a1b93788aa33e0548ba605a5a6d0bcc2e66541ed
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-10-04 07:47:34 +0530

    feat: update server configuration and axios base URL for improved environment handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoutes from './routes/authRoute.js';
 import chatRoutes from './routes/chatRoute.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import connectDB from './lib/db.js';
 
@@ -13,6 +14,7 @@ import connectDB from './lib/db.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -28,6 +30,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chat', chatRoutes);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 
 
